198e009d90c85635b26fa31a0fba7a409fff36ac
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-11-21 18:48:44 +0000

    Created: Bubble sort file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ *bubble_sort - Sorts elements by bubbling.
+ *@array: The array to be sorted.
+ *@size: The size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int holder = 0;
+	size_t  i, j = 0;
+
+	for i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				holder = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = holder;
+				print_array(array, size);
+			}
+		}
+	}
+}
